918b13521900e4426f2d10ff4df1045692105d82
abcsml <a338383154@163.com>
2023-04-28 11:27:17 +0800

    fix: add sPMP in CSR.scala

--- src/main/scala/xiangshan/backend/fu/CSR.scala
+++ src/main/scala/xiangshan/backend/fu/CSR.scala
@@ -326,8 +326,10 @@ class CSR(implicit p: Parameters) extends FunctionUnit with HasCSRConst with PMP
   // PMP Mapping
   val pmp = Wire(Vec(NumPMP, new PMPEntry())) // just used for method parameter
   val pma = Wire(Vec(NumPMA, new PMPEntry())) // just used for method parameter
+  val spmp = Wire(Vec(NumSPMP, new PMPEntry())) // just used for method parameter
   val pmpMapping = pmp_gen_mapping(pmp_init, NumPMP, PmpcfgBase, PmpaddrBase, pmp)
   val pmaMapping = pmp_gen_mapping(pma_init, NumPMA, PmacfgBase, PmaaddrBase, pma)
+  val spmpMapping = pmp_gen_mapping(pmp_init, NumSPMP, SpmpcfgBase, SpmpaddrBase, spmp, true)
 
   // Superviser-Level CSRs
 
@@ -663,6 +665,7 @@ class CSR(implicit p: Parameters) extends FunctionUnit with HasCSRConst with PMP
                 perfCntMapping ++
                 pmpMapping ++
                 pmaMapping ++
+                spmpMapping ++
                 (if (HasFPU) fcsrMapping else Nil) ++
                 (if (HasCustomCSRCacheOp) cacheopMapping else Nil)
 
